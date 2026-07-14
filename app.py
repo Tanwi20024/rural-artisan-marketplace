f81960378b1ff171a51103d8318a3cbcabf6e7fe
Tanwi20024 <tanwisrivastava2018@gmail.com>
2026-07-14 11:55:28 +0530

    Step 9: Add About and Contact pages

--- app.py
+++ app.py
@@ -40,6 +40,14 @@ def create_app():
     @app.route('/')
     def home():
         return render_template('home.html')
+        
+    @app.route('/about')
+    def about():
+        return render_template('about.html')
+
+    @app.route('/contact')
+    def contact():
+        return render_template('contact.html')
 
     return app
 
